4e64a0b78a7ab25792a3a1847abcc14cec1a1a12
AleyAleyAley <202177N@student.hci.edu.sg>
2024-11-24 23:22:46 +0800

    fix path in index.vue

--- pages/index.vue
+++ pages/index.vue
@@ -44,8 +44,8 @@
     <div>
       <section>
         <Letter>
-          <img src="/images/profiles/organising-team/co-chairpersons.webp" alt="Jacinda Kung Jiexin and Seah Yuheng"
-            class="cochairs-photo" />
+          <img src="/public/images/profiles/organising-team/co-chairpersons.webp"
+            alt="Jacinda Kung Jiexin and Seah Yuheng" class="cochairs-photo" />
           <p class="sign">Dear Student Delegates and Educators,</p>
           <p>
             Welcome to the 17th International Science Youth Forum (ISYF) @
