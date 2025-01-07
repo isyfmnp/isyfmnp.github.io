a6d66ec8c6674bbf16ba505558228583171fc3d5
AleyAleyAley <202177N@student.hci.edu.sg>
2025-01-07 09:07:45 +0800

    Day 1 Photos

--- pages/photos.vue
+++ pages/photos.vue
@@ -320,7 +320,7 @@
 <script setup>
 import { ref, nextTick, watch, onMounted, onUnmounted } from "vue";
 
-const totalDays = 1;
+const totalDays = 2;
 const currentDay = ref(1);
 const sliderLoading = ref(false)
 
